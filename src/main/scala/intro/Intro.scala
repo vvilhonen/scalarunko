82c1e260c14d1b6734df84c34b37d9d1aec7ab84
Vesa Vilhonen <vesa@vilhonen.com>
2013-01-21 13:57:13 +0200

    Some scala sources for testing

---
+++ src/main/scala/intro/Intro.scala
@@ -0,0 +1,10 @@
+package intro
+
+object Intro extends App {
+  val f = new Intro
+  println("Foo: " + f.foo)
+}
+
+class Intro {
+	def foo = 1
+}
---
+++ src/test/scala/intro/IntroTest.scala
@@ -0,0 +1,11 @@
+package intro
+
+import org.scalatest.FunSuite
+import org.scalatest.matchers.ShouldMatchers
+
+class IntroTest extends FunSuite with ShouldMatchers {
+	test("doing something") {
+	  val f = new Intro
+	  f.foo should be(1)
+	}
+}
