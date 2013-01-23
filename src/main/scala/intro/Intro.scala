0a8274cd8e6b820725f2760875872c48be690c4d
Vesa Vilhonen <vesa@vilhonen.com>
2013-01-23 11:52:46 +0200

    Update

--- src/main/scala/intro/Intro.scala
+++ src/main/scala/intro/Intro.scala
@@ -1,10 +1,48 @@
 package intro
 
-object Intro extends App {
-  val f = new Intro
-  println("Foo: " + f.foo)
+class ResultSet {
+  def close() { println("resultset closed") }
+  def getResult = "resultti"
 }
 
-class Intro {
-  def foo = 1
+class Connection {
+  def close() { println("conn closed") }
+  def query(sql: String) = {
+    println("querying " + sql)
+    new ResultSet
+  }
+}
+
+class Pool {
+  def getConnection() = {
+    println("opening connection")
+    new Connection
+  }
+}
+
+class JdbcTemplate(pool: Pool) {
+  def first[T](handler: (String) => T): T = {
+    val conn = pool.getConnection()
+    val resultSet = conn.query("select 1")
+    try {
+      handler(resultSet.getResult)
+    } finally {
+      resultSet.close()
+      conn.close()
+    }
+  }
+}
+
+object Foo {
+  def main(args: Array[String]) {
+    val jdbc = new JdbcTemplate(new Pool)
+    val intArvo = jdbc.first[Int]((x) => x)
+    //
+    // List<String> lista = new ArrayList<String>()
+    //val lista: List[String] = List[String]("a")
+
+    //jdbc.first(println(_))
+    //jdbc.first((result) => println(result))
+    //jdbc.first{(result) => println(result)}
+  }
 }
