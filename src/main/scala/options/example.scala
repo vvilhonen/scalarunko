5e16c1bb4ef0805987f046e690589ffc2bd3f593
Vesa Vilhonen <vesa@vilhonen.com>
2013-01-23 09:19:07 +0200

    Example of Option usage

---
+++ src/main/scala/options/example.scala
@@ -0,0 +1,38 @@
+class OldSchool(firstName: String, lastName: String, age: Int) {
+  override def toString: String = {
+    var firstNameStr = ""
+    if (firstName != null) {
+      firstNameStr = firstName
+    } else {
+      return "missing required values"
+    }
+
+    var lastNameStr = ""
+    if (lastName != null) {
+      lastNameStr = lastName
+    } else {
+      return "missing required values"
+    }
+
+    var ageStr = ""
+    if (age != null) {
+      ageStr = age + " years old"
+    } else {
+      ageStr = "with age omitted"
+    }
+
+    firstNameStr + " " + lastNameStr + " " + ageStr
+  }
+}
+
+class Leet(firstName: Option[String], lastName: Option[String], age: Option[Int]) {
+  override def toString: String = {
+    val result = for {
+      firstNameStr <- firstName
+      lastNameStr <- lastName
+      ageStr = age.map(_ + " years old").getOrElse("with age omitted")
+    } yield firstNameStr + " " + lastNameStr + " " + ageStr
+
+    result.getOrElse("missing required values")
+  }
+}
